e8237d01201a19d9f9bc889bceea758c0beb8155
sundaechen <sundaechen@gmail.com>
2018-09-21 01:17:28 +0800

    update code for hex pattern
    add blanked point before and after the hole pattern

--- Matlab/PhC/+PolygonFun/hexagon_array_holes_stream.m
+++ Matlab/PhC/+PolygonFun/hexagon_array_holes_stream.m
@@ -3,14 +3,19 @@ function hexagon_array_holes_stream(radius, lattice_param, structure_radius, w_b
 % and put it in filename with formatting for FEI stream file
 % Radius and lattice_param in um
 
+% 1st param: dwell time in unit of 100ns 
+% 2nd param: x position
+% 3rd param: y position
+% 4th param(optical): beam blanked 0, unblanked 1
+
 % Open file to write polygons
 fid = fopen(filename, 'w');
 
 % Format 
 format1 = ' %.0f'; % position in intergers
-format2 = strcat(num2str(dwell_time/0.1), format1, format1, ' \n');
+format2 = strcat(num2str(dwell_time/0.1), format1, format1, format1, ' \n');
 
-if isempty(findobj('Title', 'Mask')),
+if isempty(findobj('Title', 'Mask'))
     ax = gca;
     title(ax, 'Mask');
 end
@@ -34,16 +39,15 @@ for j = -2*nholes:2*nholes
     for i = -(2*nholes+mod(j,2)/2):(2*nholes+mod(j,2)/2)
         if i^2+(j*sin(pi/3))^2<=(structure_radius)^2/lattice_param^2
             % Create points
-            [c, total_points] = PolygonFun.polygoncircles_stream(lattice_param_x*i, lattice_param_y*j, ...
+            [c, cbeam, total_points] = PolygonFun.polygoncircles_stream(lattice_param_x*i, lattice_param_y*j, ...
                                                                  radius, w_beam, hollow);
             n_points = n_points+total_points;
             
             % Draw lattice in matlab for verification 
             rectangle('Position',[lattice_param_x*i,lattice_param_y*j, 2*radius, radius],'Curvature',[1 1]);
-        
 
 %         % Write points into stream file
-        fprintf(fid, format2, c/resolution);
+        fprintf(fid, format2, [c/resolution; cbeam]);
         end
     end
 end
--- Matlab/PhC/+PolygonFun/polygoncircles_stream_old.m
+++ Matlab/PhC/+PolygonFun/polygoncircles_stream_old.m
@@ -1,71 +1,61 @@
-function [c, cbeam, total_points] = polygoncircles_stream(x,y,radius,w_beam,hollow)
-%     hollow = 1;
+function [c, total_points] = polygoncircles_stream(x,y,radius,w_beam,hollow)
+%     hollow = 0;
 %     x = 0;
 %     y = 0;
 %     w_beam = 0.133;
 %     radius = 0.360-0.5*w_beam;
 % fprintf('x:%.3f y:%.3f \n',x,y)
-
+    x = x;
+    y = y;
     if hollow == 1
         radius_start = radius - 0.5*w_beam;
     else
         radius_start = 0;
     end
 
-    %% calculate number of points 
-    % additional point added before and after to blank the beam
-    total_points = 2;
+    total_points = 1;
     for l = radius_start : 0.5*w_beam :radius
         total_points = total_points+floor(4*pi*l/w_beam);
     end
 %     disp(['total points:', num2str(total_points)])
 
-  %% display beam positios for a circle
-%     if isempty(findobj('Title', 'Mask'))
-%         ax = gca;
-%         title(ax, 'Mask');
-%     end
-%     hold on
-%     axis('equal');
-%     color = linspace(0,1,total_points);
+  % display beam positios for a circle
+    if isempty(findobj('Title', 'Mask')),
+        ax = gca;
+        title(ax, 'Mask');
+    end
+    hold on
+    axis('equal');
+    color = linspace(0,1,total_points);
 
     %%%
     cx = zeros(total_points,1);
     cy = zeros(total_points,1);
-    cbeam = zeros(total_points,1);
 
-    %% add point before pattern
     i = 1;
     cx(i) = x;
     cy(i) = y;
 
 %     scatter(x,y,[],color(i), 'filled')
 %     rectangle('Position',[x-0.5*w_beam,y-0.5*w_beam,w_beam,w_beam],'Curvature',[1 1])
-    %% pattern of hole
+
     for l = radius_start : 0.5*w_beam :radius
         n_points = floor(4*pi*l/w_beam);
         for j = 0:2*pi/n_points:2*pi-2*pi/n_points
-%             pause
             i = i+1;
-            % add x, y position
             cx(i) = x + l*cos(j);
             cy(i) = y + l*sin(j);
-            cbeam(i) = 1; % unblank the beam
 %             rectangle('Position',[cx(i)-0.5*w_beam,cy(i)-0.5*w_beam,w_beam,w_beam],'Curvature',[1 1])
 %             scatter(cx(i),cy(i),[],color(i), 'filled')
         end
     end
-    
-    %% add point to blank the beam   
-    i = i+1;
-    cx(i) = x;
-    cy(i) = y;    
-%     scatter(cx(i),cy(i),[],color(i), 'filled')    
-%     pause
 
 %     hold off
 
-    c = [cx'; cy'];
-    cbeam = cbeam';
+%     color = linspace(1,10,total_points);
+%     scatter(cx,cy,[],color,'filled')
+%     axis('equal');   
+% 
+    c = [cx';cy'];
 end
     
